30584e2b0a75d469a1a3029e710700c7df56b9c3
SviatikKh <sviatoslavhr@gmail.com>
2019-11-13 00:05:16 +0200

    Homework L7 Class

---
+++ H_class.py
@@ -0,0 +1,14 @@
+class Point2:
+    def __init__(self, a, b):
+        self.a = 5
+        self.b = 10
+
+class Point3:
+    def __init__(self, c, d):
+        self.c = 15
+        self.d = 20
+
+class Circle(kolo):
+    pass
+
+
